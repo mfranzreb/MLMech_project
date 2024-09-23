9a529fd65bd1dae93dd1e845a6ccd0235138ada6
Marco Franzreb <franzrebmarco@gmail.com>
2024-09-23 16:41:23 +0200

    Add new plots

--- plot.py
+++ plot.py
@@ -16,23 +16,38 @@ if __name__ == "__main__":
     marker_map = dict(zip(pd.unique(df["model_type"]), markers))
     marker_map_lr = dict(zip(pd.unique(df["learning_rate"]), markers))
     # remove rows with "kaggle_score" > 1000
-    df = df[df["kaggle_test_score"] < 30]
-    for model_type, group3 in df.groupby("model_type"):
-        plt.figure(figsize=(10, 5))
-        for lr, group2 in group3.groupby("learning_rate"):
-            print(group2)
-            plt.scatter(
-                group2["num_params"],
+    df = df[df["num_params"] == 3000]
+    fig, axs = plt.subplots(1, 2, figsize=(10, 5))
+    for reg, group in df.groupby("regularization"):
+        for lr, group2 in group.groupby("learning_rate"):
+            axs[0].scatter(
+                group2["learning_rate"],
                 group2["kaggle_test_score"],
-                label=f"lr={lr}",
-                color=color_map[lr],
-                marker=marker_map_lr[lr],
+                color="r" if reg == 0.0 else "b",
                 alpha=0.5,
             )
-        plt.suptitle(f"Kaggle score vs number of parameters ({model_type})")
-        plt.xlabel("Number of parameters")
-        plt.ylabel("Kaggle test score")
-        # show legend for the first plot only
-        plt.legend(loc=(0.6, 0.6))
+            axs[1].scatter(
+                group2["learning_rate"],
+                group2["training_time"],
+                color="r" if reg == 0.0 else "b",
+                alpha=0.5,
+            )
+        plt.suptitle(f"Effect of regularization")
+        axs[0].set_xlabel("Learning rate")
+        axs[0].set_ylabel("Kaggle test score")
+        axs[1].set_xlabel("Learning rate")
+        axs[1].set_ylabel("Training time (s)")
+        # add legend where regularization is indicated by color if True: blue, else red
+        axs[0].legend(
+            [
+                matplotlib.lines.Line2D(
+                    [0], [0], marker="o", color="w", markerfacecolor="r", markersize=10
+                ),
+                matplotlib.lines.Line2D(
+                    [0], [0], marker="o", color="w", markerfacecolor="b", markersize=10
+                ),
+            ],
+            ["Regularization = False", "Regularization = True"],
+        )
         plt.grid()
-        plt.savefig(f"score_{model_type}.png", dpi=300)
+        plt.savefig(f"results/regularization.png", dpi=300)
